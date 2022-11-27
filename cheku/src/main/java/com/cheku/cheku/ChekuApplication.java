dda0d179b9a6725a5775ecf05603062d2f9f2876
Emanuel <emanuel.gmarques@ua.pt>
2022-11-27 23:14:09 +0000

    some dockerization working

--- cheku/src/main/java/com/cheku/cheku/ChekuApplication.java
+++ cheku/src/main/java/com/cheku/cheku/ChekuApplication.java
@@ -2,12 +2,19 @@ package com.cheku.cheku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ChekuApplication {
+	
+	@RequestMapping("/")
+	public String home() {
+	  return "Hello Docker World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChekuApplication.class, args);
 	}
-
 }
