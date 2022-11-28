3a31b7e97c099b6b47385ac21b227dd88103b07d
mariana <marianaandrade@ua.pt>
2022-11-28 16:59:16 +0000

    repository

---
+++ cheku/src/main/java/com/cheku/cheku/repository/GroupRepository.java
@@ -0,0 +1,9 @@
+package com.cheku.cheku.repository;
+
+import com.cheku.cheku.model.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public class GroupRepository extends JpaRepository<Group, Long> {
+
+}
---
+++ cheku/src/main/java/com/cheku/cheku/repository/VelocityRepository.java
@@ -0,0 +1,9 @@
+package com.cheku.cheku.repository;
+
+import com.cheku.cheku.model.Velocity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public class VelocityRepository extends JpaRepository<Velocity, Long> {
+
+}
