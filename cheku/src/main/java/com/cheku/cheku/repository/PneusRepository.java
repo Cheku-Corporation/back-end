d4d13aeac8cdc5ad6b7aa4d7d5eeb8435d7df8bd
mariana <marianaandrade@ua.pt>
2022-11-28 12:31:17 +0000

    Repository model

---
+++ cheku/src/main/java/com/cheku/cheku/repository/PneusRepository.java
@@ -0,0 +1,8 @@
+package com.cheku.cheku.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public class PneusRepository extends JpaRepository<Pneus, Long>  {
+
+}
