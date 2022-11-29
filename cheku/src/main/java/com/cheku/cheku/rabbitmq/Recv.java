eef8315f86861935246a5a666b23c2ab5fa68bd4
Diogo <d.magalhaes@ua.pt>
2022-11-29 01:13:15 +0000

    go back

--- cheku/src/main/java/com/cheku/cheku/rabbitmq/Recv.java
+++ cheku/src/main/java/com/cheku/cheku/rabbitmq/Recv.java
@@ -1,40 +1,40 @@
-package com.cheku.cheku.rabbitmq;
+// package com.cheku.cheku.rabbitmq;
 
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
+// import java.io.IOException;
+// import java.util.concurrent.TimeoutException;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+// import org.springframework.boot.CommandLineRunner;
+// import org.springframework.stereotype.Component;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DeliverCallback;
+// import com.rabbitmq.client.Channel;
+// import com.rabbitmq.client.Connection;
+// import com.rabbitmq.client.ConnectionFactory;
+// import com.rabbitmq.client.DeliverCallback;
 
-@Component
-public class Recv implements CommandLineRunner{
-    private final static String QUEUE_NAME = "car_0";
+// @Component
+// public class Recv implements CommandLineRunner{
+//     private final static String QUEUE_NAME = "car_0";
 
-    public void recv_msg() throws IOException, TimeoutException{
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
+//     public void recv_msg() throws IOException, TimeoutException{
+//         ConnectionFactory factory = new ConnectionFactory();
+//         factory.setHost("localhost");
+//         Connection connection = factory.newConnection();
+//         Channel channel = connection.createChannel();
 
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+//         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 
-        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+//         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), "UTF-8");
-            System.out.println(" [x] Received '" + message + "'");
-        };
-        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
-    }
+//         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+//             String message = new String(delivery.getBody(), "UTF-8");
+//             System.out.println(" [x] Received '" + message + "'");
+//         };
+//         channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+//     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        recv_msg();
+//     @Override
+//     public void run(String... args) throws Exception {
+//         recv_msg();
         
-    }
-}
+//     }
+// }
--- cheku/src/main/java/com/cheku/cheku/rabbitmq/Runner.java
+++ cheku/src/main/java/com/cheku/cheku/rabbitmq/Runner.java
@@ -1,25 +1,25 @@
-// package com.cheku.cheku.rabbitmq;
+package com.cheku.cheku.rabbitmq;
 
-// import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeUnit;
 
-// import org.springframework.amqp.rabbit.core.RabbitTemplate;
-// import org.springframework.boot.CommandLineRunner;
-// import org.springframework.stereotype.Component;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
-// @Component
-// public class Runner implements CommandLineRunner {
+@Component
+public class Runner implements CommandLineRunner {
 
-//   private final RabbitTemplate rabbitTemplate;
-//   private final Receiver receiver;
+  private final RabbitTemplate rabbitTemplate;
+  private final Receiver receiver;
 
-//   public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
-//     this.receiver = receiver;
-//     this.rabbitTemplate = rabbitTemplate;
-//   }
+  public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
+    this.receiver = receiver;
+    this.rabbitTemplate = rabbitTemplate;
+  }
 
-//   @Override
-//   public void run(String... args) throws Exception {
-//     receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
-//   }
+  @Override
+  public void run(String... args) throws Exception {
+    receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+  }
 
-// }
+}
