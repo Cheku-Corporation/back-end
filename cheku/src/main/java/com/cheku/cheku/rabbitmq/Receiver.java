8f2ebd27464aa97b3212f9009d9d2e10aeb7a31a
Diogo <d.magalhaes@ua.pt>
2022-11-29 01:02:30 +0000

    Trying to receive data

--- cheku/src/main/java/com/cheku/cheku/rabbitmq/Receiver.java
+++ cheku/src/main/java/com/cheku/cheku/rabbitmq/Receiver.java
@@ -1,19 +1,19 @@
-package com.cheku.cheku.rabbitmq;
+// package com.cheku.cheku.rabbitmq;
 
-import java.util.concurrent.CountDownLatch;
-import org.springframework.stereotype.Component;
+// import java.util.concurrent.CountDownLatch;
+// import org.springframework.stereotype.Component;
 
-@Component
-public class Receiver {
+// @Component
+// public class Receiver {
 
-  private CountDownLatch latch = new CountDownLatch(1);
+//   private CountDownLatch latch = new CountDownLatch(1);
 
-  public void receiveMessage(String message) {
-    System.out.println("Received <" + message + ">");
-    latch.countDown();
-  }
+//   public void receiveMessage(String message) {
+//     System.out.println("Received <" + message + ">");
+//     latch.countDown();
+//   }
 
-  public CountDownLatch getLatch() {
-    return latch;
-  }
-}
+//   public CountDownLatch getLatch() {
+//     return latch;
+//   }
+// }
