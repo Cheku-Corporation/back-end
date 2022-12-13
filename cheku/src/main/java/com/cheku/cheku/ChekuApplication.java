5546415d2405a216515c7ec157a342b4b964798e
v1centebarros <vmabarros@gmail.com>
2022-12-13 15:42:55 +0000

    CHEK-74 minor fix

--- cheku/src/main/java/com/cheku/cheku/ChekuApplication.java
+++ cheku/src/main/java/com/cheku/cheku/ChekuApplication.java
@@ -2,42 +2,14 @@ package com.cheku.cheku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @EnableWebMvc
 @Configuration
 @SpringBootApplication
 public class ChekuApplication implements WebMvcConfigurer {
-
-//  @Override
-//  public void addCorsMappings(CorsRegistry registry) {
-//      registry.addMapping("*");
-//  }
-
-  @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-    final CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://localhost:5173"));
-    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
-    // setAllowCredentials(true) is important, otherwise:
-    // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
-    configuration.setAllowCredentials(true);
-    // setAllowedHeaders is important! Without it, OPTIONS preflight request
-    // will fail with 403 Invalid CORS request
-    configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
-  }
-  
   public static void main(String[] args) {
     SpringApplication.run(ChekuApplication.class, args);
   }
