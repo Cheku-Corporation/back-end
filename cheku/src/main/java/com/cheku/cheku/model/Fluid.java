36638bdb60b2f1d5882230357e2c1b8f7ee69181
mariana <marianaandrade@ua.pt>
2022-12-20 23:37:04 +0000

    fluid and ligths comments

--- cheku/src/main/java/com/cheku/cheku/model/Fluid.java
+++ cheku/src/main/java/com/cheku/cheku/model/Fluid.java
@@ -1,41 +1,58 @@
 package com.cheku.cheku.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.*;
-import java.util.Date;
 
-import javax.validation.constraints.NotNull;
-
+/**
+ * Entity class for storing fluid data.
+ */
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Table(name = "fluid")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Fluid {
 
+    /** The unique ID of the fluid.*/
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true)
     private Long id;
 
+    /** The fuel level of the fluid. */
     @Column(name = "fuel", nullable = true)
     private Double fuel;
 
+    /** The oil level of the fluid. */
     @NotNull(message = "Oil is required")
     @Column(name = "oil", nullable = false)
     private Double oil;
 
+    /** The water level of the fluid.*/
     @NotNull(message = "Water is required")
     @Column(name = "water", nullable = false)
     private Double water;
 
+    /** The date when the fluid data was recorded. */
     @NotNull(message = "Date is required")
     @Column(name = "date", nullable = false)
     private Date date;
 
+    /** The trip that the fluid data is related to.*/
     @ManyToOne(optional = true)
     @JoinColumn(name = "trip_id", nullable = true)
     private Trip trip;
-
 }
--- cheku/src/main/java/com/cheku/cheku/model/LightsHistory.java
+++ cheku/src/main/java/com/cheku/cheku/model/LightsHistory.java
@@ -1,31 +1,46 @@
 package com.cheku.cheku.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
-
-import javax.persistence.*;
 import com.cheku.cheku.model.enums.LightState;
 
+/**
+ * Entity class for storing lights history data.
+ */
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Table(name = "luzes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LightsHistory {
 
+    /** The unique ID of the lights history record. */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /** The state of the lights. */
     @Column(name = "state", nullable = false)
     private LightState state;
 
+    /** The date when the lights state was recorded. */
     @Column(name = "date", nullable = false)
     private Date date;
-    
+
+    /** The trip that the lights history is related to. */
     @ManyToOne(optional = true)
     @JoinColumn(name = "trip_id", nullable = true)
     private Trip trip;
