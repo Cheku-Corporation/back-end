307f02bc3c7c6131bafc409a0e9d63d853a1e6ee
mariana <marianaandrade@ua.pt>
2022-12-20 23:40:13 +0000

    notification comments

--- cheku/src/main/java/com/cheku/cheku/model/Notification.java
+++ cheku/src/main/java/com/cheku/cheku/model/Notification.java
@@ -1,37 +1,50 @@
 package com.cheku.cheku.model;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
+/**
+ * Entity class for storing notification data.
+ */
 @Entity
-@Data
 @Table(name = "notifications")
-@AllArgsConstructor
+@Data
 @NoArgsConstructor
-public class Notification  {
+@AllArgsConstructor
+public class Notification {
 
+   /** The unique ID of the notification. */
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
 
+   /** The priority of the notification. */
    @Column(name = "priority", nullable = false)
    private Integer priority;
 
+   /** The subject of the notification. */
    @Column(name = "subject", nullable = false)
    private String subject;
 
+   /** The message of the notification. */
    @Column(name = "message", nullable = false)
    private String message;
 
+   /** The type of the notification. */
    @Column(name = "type", nullable = false)
    private String type;
 
-   // @Column(name = "date", nullable = false)
-   // private Date date;
-
+   /** The car that the notification is related to. */
    @ManyToOne(optional = true)
    @JoinColumn(name = "car_id", nullable = true)
    private Car car;
