bb1791fe8e23bbaa178cfbc9723c1bedbcdf1ea9
mariana <marianaandrade@ua.pt>
2022-12-20 17:23:37 +0000

    apiUser comment and clean

--- cheku/src/main/java/com/cheku/cheku/model/ApiUser.java
+++ cheku/src/main/java/com/cheku/cheku/model/ApiUser.java
@@ -1,39 +1,55 @@
 package com.cheku.cheku.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.*;
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
+import lombok.*;
+
+/**
+ * Entity class for storing user data in the database.
+ */
 @Entity
+@Table(name = "users")
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "users")
+@AllArgsConstructor
 public class ApiUser {
 
-    //dados estaticos
+    /**
+     * The unique identifier for this user in the database.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * The user's email address.
+     */
     @Column(name = "email", nullable = false)
     private String email;
 
+    /**
+     * The user's password (encrypted).
+     */
     @Column(name = "password", nullable = false)
     private String password;
 
+    /**
+     * The user's name.
+     */
     @Column(name = "name", nullable = false)
     private String name;
 
+    /**
+     * The user's role (e.g. "admin", "moderator", "user").
+     */
     @Column(name = "role", nullable = false)
     private String role;
 
-    @ManyToOne()
+    /**
+     * The group that this user belongs to.
+     */
+    @ManyToOne
     @JoinColumn(name = "group_id")
     private Group group;
-
 }
