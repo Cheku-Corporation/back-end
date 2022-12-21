a2746f12352f9cff621b4cb02f637e32ea988bcc
mariana <marianaandrade@ua.pt>
2022-12-21 01:15:38 +0000

    service carModel

--- cheku/src/main/java/com/cheku/cheku/service/CarModelService.java
+++ cheku/src/main/java/com/cheku/cheku/service/CarModelService.java
@@ -30,16 +30,11 @@ public class CarModelService {
      * @return a list of CarModelDTO objects
      */
     public List<CarModelDTO> getAllCarModels() {
-        // Find all car models
         List<CarModel> cars = carModelRepository.findAll();
-        // Create an empty list of CarModelDTO objects
         List<CarModelDTO> carModels = new ArrayList<>();
-        // Map each car model to a CarModelDTO object and add it to the list
         for (CarModel car : cars) {
-            CarModelDTO carModelDTO =  modelMapper.map(car, CarModelDTO.class);
-            carModels.add(carModelDTO);
+            carModels.add(modelMapper.map(car, CarModelDTO.class));
         }
-        // Return the list of CarModelDTO objects
         return carModels;
     }
 
@@ -50,27 +45,23 @@ public class CarModelService {
      * @throws ResourceNotFoundException if a car model with the same brand, model, year, tank capacity, type, motor, and tires already exists, or if the motor or tires do not exist
      */
     public CarModel createCarModel(CarModel car) throws ResourceNotFoundException {
-        // Check if a car model with the same brand, model, year, tank capacity, type, motor, and tires already exists
         if (carModelRepository.findByBrandAndModelAndYearAndTankCapacityAndTypeAndMotorAndTires(car.getBrand(), car.getModel(), car.getYear(), car.getTankCapacity(), car.getType(), car.getMotor(), car.getTires()) != null) {
-            // If a car model with the same details already exists, throw a ResourceNotFoundException
             throw new ResourceNotFoundException("The Model car already exists");
         }
         // Check if the motor exists
         else if (motorRepository.findById(car.getMotor().getId()) == null) {
-             // If the motor does not exist, throw a Resource otFoundException
-            throw new ResourceNotFoundException("The motor doesn't exist");
+                throw new ResourceNotFoundException("The motor doesn't exist");
             }
             // Check if the tires exist
             else if (pneusRepository.findById(car.getTires().getId()) == null) {
-            // If the tires do not exist, throw a ResourceNotFoundException
-            throw new ResourceNotFoundException("The pneus doesn't exist");
+                throw new ResourceNotFoundException("The pneus doesn't exist");
             }
             try{
-            // Find the motor and tires by their ID
-            car.setMotor(motorRepository.findById(car.getMotor().getId()).get());
-            car.setTires(pneusRepository.findById(car.getTires().getId()).get());
-            // Save the car model and return it
-            return carModelRepository.save(car);
+                // Find the motor and tires by their ID
+                car.setMotor(motorRepository.findById(car.getMotor().getId()).get());
+                car.setTires(pneusRepository.findById(car.getTires().getId()).get());
+                // Save the car model and return it
+                return carModelRepository.save(car);
             } catch (Exception e) {
             // If an error occurs while saving the car model, throw a ResourceNotFoundException
             throw new ResourceNotFoundException("Error saving car");
