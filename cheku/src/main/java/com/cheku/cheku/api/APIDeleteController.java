e1b2d840bbc060926e23b78ecf75af7c7754a29d
mariana <marianaandrade@ua.pt>
2022-12-21 00:45:49 +0000

    delete car controller

--- cheku/src/main/java/com/cheku/cheku/api/APIDeleteController.java
+++ cheku/src/main/java/com/cheku/cheku/api/APIDeleteController.java
@@ -1,11 +1,13 @@
 package com.cheku.cheku.api;
 
-
 import com.cheku.cheku.exception.ResourceNotFoundException;
+import com.cheku.cheku.model.Group;
+import com.cheku.cheku.service.CarService;
+import com.cheku.cheku.service.GroupService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import com.cheku.cheku.service.*;
-import com.cheku.cheku.model.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class APIDeleteController {
@@ -16,21 +18,38 @@ public class APIDeleteController {
     @Autowired
     private GroupService groupService;
 
-    @DeleteMapping("api/user/{user_id}/group/{group_id}/car/{car_id}")
-    public Object deleteCar(@PathVariable Long car_id, @PathVariable Long group_id, @PathVariable Long user_id ) throws ResourceNotFoundException {
-        //verificar se o user_id é o admin do grupo
-        if (groupService.verifyAdmin(user_id, group_id)) {
-            carService.deleteCar(car_id);
-            Object response = new Object() {
-                public final String sucesso = "Carro apagado com sucesso";
-            };
-            return response;
+    @DeleteMapping("api/user/{userId}/group/{groupId}/car/{carId}")
+    public Object deleteCar(@PathVariable Long carId, @PathVariable Long groupId, @PathVariable Long userId) throws ResourceNotFoundException {
+        Group group = groupService.getGroupById(groupId);
+        if (groupService.verifyAdmin(userId, groupId)) {
+            carService.deleteCar(carId);
+            return new SuccessResponse("Carro apagado com sucesso");
         } else {
-            Object response = new Object() {
-                public final String message = "Não tem permissões para apagar o carro";
-            };
-            return response;
+            return new ErrorResponse("Não tem permissões para apagar o carro");
         }
     }
 
+    private static class SuccessResponse {
+        private String success;
+
+        public SuccessResponse(String success) {
+            this.success = success;
+        }
+
+        public String getSuccess() {
+            return success;
+        }
+    }
+
+    private static class ErrorResponse {
+        private String message;
+
+        public ErrorResponse(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
 }
