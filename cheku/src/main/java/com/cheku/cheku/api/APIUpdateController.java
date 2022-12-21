2f7b0db14e5f64fd021cb688ac17a74448bb0e2c
mariana <marianaandrade@ua.pt>
2022-12-21 00:56:12 +0000

    edit car

--- cheku/src/main/java/com/cheku/cheku/api/APIUpdateController.java
+++ cheku/src/main/java/com/cheku/cheku/api/APIUpdateController.java
@@ -7,47 +7,62 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import com.cheku.cheku.model.Car;
-import com.cheku.cheku.service.CarService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+import com.cheku.cheku.model.Car;
+import com.cheku.cheku.service.CarService;
 
+/**
+ * Rest controller for handling requests to updates.
+ */
 @RestController
 @RequestMapping("/api")
 public class APIUpdateController {
 
-    @Autowired
-    private CarService carService;
+    private final CarService carService;
+    private final GroupService groupService;
+    private final UserService userService;
 
     @Autowired
-    private GroupService groupService;
-
-    @Autowired
-    private UserService userService;
-
+    public APIUpdateController(CarService carService, GroupService groupService, UserService userService) {
+        this.carService = carService;
+        this.groupService = groupService;
+        this.userService = userService;
+    }
+    /** Updates a car with the given data. */
     @PutMapping("user/group/car/{id}")
     public Car updateCar(@PathVariable Long id, @Valid @RequestBody String data) throws ResourceNotFoundException, JsonProcessingException {
+        // Set up an object mapper to parse the data
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
+        // Get the current car and the updated car data
         Car car = carService.getCarById(id);
-
         Car carUpdate = mapper.readValue(data, Car.class);
 
+        // Get the group ID and user ID from the data
         Long groupId = mapper.readTree(data).get("group").get("id").asLong();
         Long userId = mapper.readTree(data).get("userId").asLong();
 
-        if (groupService.getGroupById(groupId).getIsAdmin() != userId){
+        // Check if the user is authorized to update the car
+        if (groupService.getGroupById(groupId).getIsAdmin() != userId) {
             throw new ResourceNotFoundException("Not Authorized");
         }
 
-        if (carService.existsByMatricula(carUpdate.getMatricula())){
+        // Check if the updated car's license plate already exists
+        if (carService.existsByMatricula(carUpdate.getMatricula())) {
             throw new ResourceNotFoundException("Matricula already exists");
         }
 
-        return carService.updateCar(car,carUpdate, userId);
+        // Update the car and return the result
+        return carService.updateCar(car, carUpdate, userId);
     }
-
 }
+
+
